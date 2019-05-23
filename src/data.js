4558cc74079997e52e7bedaaf9a1afda529a3276
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-05-23 01:11:10 -0400

    se agrega funcion para incluir imagenes y se modifica formato de codigo

--- src/data.js
+++ src/data.js
@@ -1,3 +1,35 @@
+const patchImage = (movie) => {
+  if (movie.Poster === 'N/A') {
+    const patches = {
+      'tt0365467': 'img/patches/matrix1999.jpg',
+      'tt0274085': 'img/patches/matrix20001.jpg',
+      'tt0224086': 'img/patches/matrix20002.jpg',
+      'tt0382814': 'img/patches/lordrings2003.jpg',
+      'tt0843849': 'img/patches/tokyodrift.jpg',
+      'tt0781022': 'img/patches/editing2002.jpg',
+      'tt0781023': 'img/patches/visualeffects2002.jpg',
+      'tt5267212': 'img/patches/dom2009.jpg',
+      'tt2327114': 'img/patches/makinghungergames2012.jpg',
+      'tt2858204': 'img/patches/katnissyrue.jpg',
+      'tt2427488': 'img/patches/hungergames2012.jpg',
+      'tt4026144': 'img/patches/mockingjay.jpg',
+      'tt3413318': 'img/patches/secondquarter2011.jpg',
+
+    }
+    return patches[movie.imdbID] || 'http://ximg.es/160x220/b8bdb8/fff.jpg&text=N/A'
+  } else {
+    return movie.Poster;
+  }
+}
+
+window.patchImage = patchImage
+
+
+
+
+
+
+
 // const movies = {
 //     harryPotter: [
 //         'tt1201607',  // Harry Potter and the Deathly Hallows: Part 2
@@ -23,6 +55,8 @@
 // }
 
 // window.filterByGenre = filterByGenre;
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 const url = 'http://www.omdbapi.com/?apikey=733596b5&type=movie&s=';
 const urlId = 'http://www.omdbapi.com/?apikey=733596b5&type=movie&plot=short&i='
 
@@ -17,227 +16,204 @@ let cardsModal = '';
 let titleMovie;
 let moviesForGenre;
 
-const arrayMovies = ["Harry Potter", "Transformers", "Star Wars", "Fast and the Furious", "Matrix","Hunger Games","The Lord of The Rings", "Star Trek"];
-
-
-
+const arrayMovies = [
+  "Harry Potter",
+  "Transformers",
+  "Star Wars",
+  "Fast and the Furious",
+  "Matrix",
+  "Hunger Games",
+  "The Lord of The Rings",
+  "Star Trek"
+];
 
 buttonConsult.addEventListener("click", () => {
-    titleMovie = document.getElementById("movies").value;
-        loadmovies(titleMovie);
-    
-    });
+  titleMovie = document.getElementById("movies").value;
+  loadmovies(titleMovie);
+});
 
 //evento para capturar la opcion del select
 buttonConsult.addEventListener("click", () => {
-titleMovie = document.getElementById("movies").value;
-    loadmovies(titleMovie);
-
+  titleMovie = document.getElementById("movies").value;
+  loadmovies(titleMovie);
 });
 
 starWarsCarouselImg.addEventListener("click", () => {
-    titleMovie = "Star Wars";
-        loadmovies(titleMovie);
-    
-    });
+  titleMovie = "Star Wars";
+  loadmovies(titleMovie);
+});
 
 harryPotterCarouselImg.addEventListener("click", () => {
-        titleMovie = "Harry Potter";
-            loadmovies(titleMovie);
-        
-        });
+  titleMovie = "Harry Potter";
+  loadmovies(titleMovie);
+});
 
-transformersCarouselImg .addEventListener("click", () => {
-    titleMovie = "Transformers";
-            loadmovies(titleMovie);
-            
-            });
+transformersCarouselImg.addEventListener("click", () => {
+  titleMovie = "Transformers";
+  loadmovies(titleMovie);
+});
+
+const loadmovies = (titleMovie) => {
+  cardsMovie = '';
+
+  //consultar a la url omdb
+  fetch(url + titleMovie)
+    .then(response => response.json())
+    .then(response => {
+      if (titleMovie !== 'Sagas') {
+        movies = response.Search;
 
 
-
-
-const loadmovies =(titleMovie)=>{
-    cardsMovie = '';
-    
-    //consultar a la url omdb
-    fetch(url + titleMovie)
-        .then(response => response.json())
-        .then(response => {
-            if (titleMovie !== 'Sagas') {
-                movies = response.Search;
-               
-
-                movies.forEach(el => {
-                    //imprimir las cartas de peliculas
-                    cardsMovie += `
-   <div class="card col-12 col-sm-4 col-md-3">
-   <a data-toggle="modal" data-target="#exampleModal${el.imdbID}">
-   <div class="card-body"> 
-   <div class = "img-movie">`
-
-                    if (el.Poster === "N/A") {
-                        cardsMovie += `
-    <img src="http://ximg.es/160x220/b8bdb8/fff.jpg&text=N/A" class="img-movie-card" alt="">`
-                    }
-                    else {
-                        cardsMovie += `
-   <img src=${el.Poster} class="img-movie-card" alt="">`
-                    }
-                    cardsMovie += ` 
-   <p class="card-text">${el.Title} ${el.Year}</p>
-    </div>
-   </div>
-   </a>
- </div>`
-                    //consultar a la url omdb por ID tomando el id de movies
-                    fetch(urlId + el.imdbID)
-                        .then(response => response.json())
-                        .then(response => {
-                            moviesModal = response;
-                            cardsModal += `  
-    <div class="modal fade" id="exampleModal${moviesModal.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md" role="document">
-    
-    <div class="modal-content">
-    <div class="x-close">                     
-  
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button>
-    </div>
-    
-    <div class="modal-body">
-    <div class = "title-modal">
-    <h5>${moviesModal.Title} ${moviesModal.Year}</h5>  
-    </div>           
-    <div class= "modal-img">
-    <img src =${moviesModal.Poster} class="img-movie-poster">
-    </div>
-    
-   <div class= "modal-img">
-    
-    <p>Sinopsis:${moviesModal.Plot} </p>
-    <p>Genero: ${moviesModal.Genre}.</p>
-    <p>Actores: <img src="./img/actors.png" alt="">${moviesModal.Actors}</p>
-    <p>Director: ${moviesModal.Director}</p>
-    <p>Premios: ${moviesModal.Awards}</p>
-    <p>Ratings: ${moviesModal.Ratings[0].Value}</p>
-    <p>Website:<a href= "${moviesModal.Website}">${moviesModal.Website}</a> </p>
-       
-    </div>
-    </div>
-                 
+        movies.forEach(el => {
+          //imprimir las cartas de peliculas
+          cardsMovie += `
+            <div class="card col-12 col-sm-4 col-md-3">
+              <a data-toggle="modal" data-target="#exampleModal${el.imdbID}">
+                <div class="card-body"> 
+                  <div class = "img-movie">
+                    <img src="${window.patchImage(el)}" class="img-movie-card" alt="">
+                    <p class="card-text">${el.Title} ${el.Year}</p>
+                  </div>
                 </div>
-            </div>
-        </div>`
-                            //Jquery del modal
-                            $('#exampleModal' + moviesModal.imdbID).on('shown.bs.modal', function () {
-                                $('#myInput').trigger('focus')
-                            });
-                            //imprimiendo el modal
-                            movieSearchId.innerHTML = cardsModal;
-                        });
+              </a>
+            </div>`;
+          //consultar a la url omdb por ID tomando el id de movies
+          fetch(urlId + el.imdbID)
+            .then(response => response.json())
+            .then(response => {
+              moviesModal = response;
+              cardsModal += `  
+                <div class="modal fade" id="exampleModal${moviesModal.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-md" role="document">
+                
+                <div class="modal-content">
+                <div class="x-close">                     
+              
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                    
+                    <div class="modal-body">
+                        <div class = "title-modal">
+                          <h5>${moviesModal.Title} ${moviesModal.Year}</h5>  
+                        </div>           
+                          <div class= "modal-img">
+                        <img src="${window.patchImage(el)}" class="img-movie-poster">
+                        </div>
+                        
+                        <div class= "modal-img">
+                        
+                        <p>Sinopsis:${moviesModal.Plot} </p>
+                        <p>Genero: ${moviesModal.Genre}.</p>
+                        <p>Actores: <img src="./img/actors.png" alt="">${moviesModal.Actors}</p>
+                        <p>Director: ${moviesModal.Director}</p>
+                        <p>Premios: ${moviesModal.Awards}</p>
+                        <p>Ratings: ${moviesModal.Ratings[0].Value}</p>
+                        <p>Website:<a href= "${moviesModal.Website}">${moviesModal.Website}</a> </p>
+                          
+                        </div>
+                    </div>
+                                
+                  </div>
+                  </div>
+                  </div>`
+                              
+                  //Jquery del modal
+                  $('#exampleModal' + moviesModal.imdbID).on('shown.bs.modal', function () {
+                  $('#myInput').trigger('focus')
+                  });
+                  //imprimiendo el modal
+                  movieSearchId.innerHTML = cardsModal;
                 });
-                //impriendo las tarjetas
-                movieSearch.innerHTML = cardsMovie;
-            }
+            });
+            //impriendo las tarjetas
+            movieSearch.innerHTML = cardsMovie;
+          }
         });
-}
+    }
 
 
 buttonGenre.addEventListener("click", () => {
-    cardsMovie='';
-    genreSelect = document.getElementById("genre").value;
-    
-     arrayMovies.forEach(element => {
-  fetch(url + element)
-        .then(response => response.json())
-        .then(response => {
-movies = response.Search;
-movies.forEach(el => {
-    fetch(urlId + el.imdbID)
-    .then(response => response.json())
-    .then(response => {
-        moviesForGenre = response;
+  cardsMovie = '';
+  genreSelect = document.getElementById("genre").value;
 
-if (moviesForGenre.Genre.includes(genreSelect)){
-    
-     cardsMovie += `
-    <div class="card col-12 col-sm-4 col-md-3">
-    <a data-toggle="modal" data-target="#exampleModal${el.imdbID}">
-    <div class="card-body"> 
-    <div class = "img-movie">`
- 
-                     if (el.Poster === "N/A") {
-                         cardsMovie += `
-     <img src="http://ximg.es/160x220/b8bdb8/fff.jpg&text=N/A" class="img-movie-card" alt="">`
-                     }
-                     else {
-                         cardsMovie += `
-    <img src=${el.Poster} class="img-movie-card" alt="">`
-                     }
-                     cardsMovie += ` 
-    <p class="card-text">${el.Title} ${el.Year}</p>
-     </div>
-    </div>
-    </a>
-  </div>`
+  arrayMovies.forEach(element => {
+    fetch(url + element)
+      .then(response => response.json())
+      .then(response => {
+        movies = response.Search;
+        movies.forEach(el => {
+          fetch(urlId + el.imdbID)
+            .then(response => response.json())
+            .then(response => {
+              moviesForGenre = response;
 
-  cardsModal+=`
+              if (moviesForGenre.Genre.includes(genreSelect)) {
+
+                cardsMovie += `
+                  <div class="card col-12 col-sm-4 col-md-3">
+                    <a data-toggle="modal" data-target="#exampleModal${el.imdbID}">
+                    <div class="card-body"> 
+                      <div class = "img-movie">
+                      <img src=${window.patchImage(el)} class="img-movie-card" alt="">
+                      <p class="card-text">${el.Title} ${el.Year}</p>
+                      </div>
+                    </div>
+                    </a>
+                  </div>`
+
+                cardsModal += `
   
-  <div class="modal fade" id="exampleModal${moviesForGenre.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-md" role="document">
-  
-  <div class="modal-content">
-  <div class="x-close">                     
+                    <div class="modal fade" id="exampleModal${moviesForGenre.imdbID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-md" role="document">
+                    
+                        <div class="modal-content">
+                        <div class="x-close">                     
 
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true">&times;</span>
-</button>
-  </div>
-  
-  <div class="modal-body">
-  <div class = "title-modal">
-  <h5>${moviesForGenre.Title} ${moviesForGenre.Year}</h5>  
-  </div>           
-  <div class= "modal-img">
-  <img src =${moviesForGenre.Poster} class="img-movie-poster">
-  </div>
-  
- <div class= "modal-img">
-  
-  <p>Sinopsis:${moviesForGenre.Plot} </p>
-  <p>Genero: ${moviesForGenre.Genre}.</p>
-  <p>Actores: <img src="./img/actors.png" alt="">${moviesForGenre.Actors}</p>
-  <p>Director: ${moviesForGenre.Director}</p>
-  <p>Premios: ${moviesForGenre.Awards}</p>
-  <p>Ratings: ${moviesForGenre.Ratings[0].Value}</p>
-  <p>Website:<a href= "${moviesForGenre.Website}">${moviesForGenre.Website}</a> </p>
-     
-  </div>
-  </div>
-               
-              </div>
-          </div>
-      </div>`
-                          //Jquery del modal
-                          $('#exampleModal' + moviesForGenre.imdbID).on('shown.bs.modal', function () {
-                              $('#myInput').trigger('focus')
-                          });
-                          //imprimiendo el modal
-                          movieSearchId.innerHTML = cardsModal;
-  
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                          </div>
+                    
+                    <div class="modal-body">
+                      <div class = "title-modal">
+                      <h5>${moviesForGenre.Title} ${moviesForGenre.Year}</h5>  
+                      </div>           
+                        <div class= "modal-img">
+                        <img src =${moviesForGenre.Poster} class="img-movie-poster">
+                    </div>
+                    
+                    <div class= "modal-img">
+                      
+                      <p>Sinopsis:${moviesForGenre.Plot} </p>
+                      <p>Genero: ${moviesForGenre.Genre}.</p>
+                      <p>Actores: <img src="./img/actors.png" alt="">${moviesForGenre.Actors}</p>
+                      <p>Director: ${moviesForGenre.Director}</p>
+                      <p>Premios: ${moviesForGenre.Awards}</p>
+                      <p>Ratings: ${moviesForGenre.Ratings[0].Value}</p>
+                      <p>Website:<a href= "${moviesForGenre.Website}">${moviesForGenre.Website}</a> </p>
+                        
+                       </div>
+                       </div>
+                                
+                        </div>
+                      </div>
+                    </div>`
+                //Jquery del modal
+                $('#exampleModal' + moviesForGenre.imdbID).on('shown.bs.modal', function () {
+                  $('#myInput').trigger('focus')
+                });
+                //imprimiendo el modal
+                movieSearchId.innerHTML = cardsModal;
 
+              }
+              movieSearch.innerHTML = cardsMovie;
+            })
+        });
 
-
-
-}
-movieSearch.innerHTML = cardsMovie;
-})  
-});
-
-});
-});
+      });
+  });
 });
 
 // const movieList = document.getElementById('movie_list');
